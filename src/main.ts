0b175d3a956dc4dfb63212be185990dc8f72472e
F3547441 Gabriel Oliveira da Silva Sergio <gabriel.sergio@bb.com.br>
2022-07-14 09:11:52 -0300

    redirected movement listener to the correct class

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,6 @@
 import { GameController } from "./controller/GameController";
 
 const game = new GameController();
-
-document.addEventListener("keydown", (event) =>{
-    if(event.key == "ArrowUp"){
-        game.snakeMove('up');
-    }
-    if(event.key == "ArrowDown"){
-        game.snakeMove('down');
-    }
-    if(event.key == "ArrowLeft"){
-        game.snakeMove('left');
-    }
-    if(event.key == "ArrowRight"){
-        game.snakeMove('right');
-    }
-});
+game.start();
 
 export{}
