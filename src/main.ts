8c62e9d75c52d2a823621a86080c0fd45bdaafbb
F3547441 Gabriel Oliveira da Silva Sergio <gabriel.sergio@bb.com.br>
2022-07-14 08:16:48 -0300

    setted up the snake move listener

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,17 @@ import { GameController } from "./controller/GameController";
 const game = new GameController();
 
 document.addEventListener("keydown", (event) =>{
+    if(event.key == "ArrowUp"){
+        game.snakeMove('up');
+    }
     if(event.key == "ArrowDown"){
         game.snakeMove('down');
+    }
+    if(event.key == "ArrowLeft"){
+        game.snakeMove('left');
+    }
+    if(event.key == "ArrowRight"){
+        game.snakeMove('right');
     }
 });
 
