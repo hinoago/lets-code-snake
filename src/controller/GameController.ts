ef7d58ce2a883c883f8b8fc01bed2ae525b0b87e
F3547441 Gabriel Oliveira da Silva Sergio <gabriel.sergio@bb.com.br>
2022-07-14 09:12:21 -0300

    redirected movement listener to the correct class

--- src/controller/GameController.ts
+++ src/controller/GameController.ts
@@ -1,3 +1,4 @@
+import { Area } from "../model/Area";
 import { Field } from "../model/Field"
 import { Snake } from "../model/Snake";
 import { View } from "../view/View";
@@ -6,15 +7,17 @@ class GameController{
     private field: Field = new Field(10, 10);
     private snake = new Snake();
 
-    constructor(){
+    start(): void{
         this.renderField();
+        this.insertFood(1000);
+        this.listenSnakeMovement();
     }
 
-    getField(){
+    getField(): Array<Area>{
         return this.field.getField();
     }
 
-    getSnakeOnField(){
+    getSnakeOnField(): Array<Area>{
         return this.getField().filter(area =>{
             const slice = this.snake.getSnake().find(pos => pos.getX() == area.getX() && pos.getY() == area.getY());
             if(slice){
@@ -25,29 +28,50 @@ class GameController{
         });
     }
 
+    listenSnakeMovement(): void{
+        document.addEventListener("keydown", (event) =>{
+            if(event.key == "ArrowUp"){
+                this.snakeMove('up')
+            }
+            if(event.key == "ArrowDown"){
+                this.snakeMove('down');
+            }
+            if(event.key == "ArrowLeft"){
+                this.snakeMove('left');
+            }
+            if(event.key == "ArrowRight"){
+                this.snakeMove('right');
+            }
+        });
+    }
+
     //TODO: Verify if the next snake's movement is inside the field
-    snakeMove(direction: 'up' | 'down' | 'left' | 'right'){
+    snakeMove(direction: 'up' | 'down' | 'left' | 'right'): void{
         this.snake.move(direction);
         this.updateField();
     }
 
-    createFood(){
+    insertFood(time: number): void{
+        setInterval(() =>{
+            this.renderFood();
+        }, time);
+    }
+
+    renderFood(): void{
         const field = this.getField();
         const emptyAreas = field.filter(area => area.getContent() == 'blank');
         const emptyArea = emptyAreas[Math.floor(Math.random()*emptyAreas.length)];
         emptyArea.setContent('food');
         this.updateField();
-        console.log(emptyArea);
     }
 
-    renderField(){
+    renderField(): void{
         const fieldLength = this.getField().length/10;
         View.renderField(fieldLength, fieldLength);
         View.renderSnake();
-        return this.field;
     }
 
-    updateField(){
+    updateField(): void{
         const snakePositionOnField = this.getSnakeOnField();
 
         //Removing snake from area
